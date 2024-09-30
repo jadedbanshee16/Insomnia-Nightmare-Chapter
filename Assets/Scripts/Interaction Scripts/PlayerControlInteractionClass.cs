0de69f6ef81d921d6c911176c05a18eabf8fa435
jadedbanshee16 <jerome.gardiner1@gmail.com>
2024-10-01 08:37:15 +1000

    player control updated
    +Allowed rotation of object to face player when using player control.

--- Assets/Scripts/Interaction Scripts/PlayerControlInteractionClass.cs
+++ Assets/Scripts/Interaction Scripts/PlayerControlInteractionClass.cs
@@ -12,12 +12,50 @@ public class PlayerControlInteractionClass : InteractionClass
     CinemachineVirtualCamera currentCam;
 
     private bool isOn;
+    private bool isAdjusting;
+
+    [SerializeField]
+    Transform adjustedObject;
+
+    Quaternion initialPosition;
+    float timer = 0;
 
     private void Start()
     {
         currentCam = GetComponentInChildren<CinemachineVirtualCamera>();
 
         setController();
+
+        //Set the initial position.
+        if (adjustedObject)
+        {
+            initialPosition = adjustedObject.rotation;
+        }
+    }
+
+    private void Update()
+    {
+        if (isAdjusting)
+        {
+            if(timer < 1)
+            {
+                timer += Time.deltaTime;
+            } else
+            {
+                timer = 1;
+                isAdjusting = false;
+            }
+
+            //Adjust connected object, given it has to face the camera.
+            if (isOn)
+            {
+                Vector3 newPos = currentCam.transform.position - adjustedObject.position;
+                controller.setPosition(adjustedObject.position, Quaternion.Slerp(initialPosition, Quaternion.Euler(newPos.x, newPos.y, newPos.z), timer), adjustedObject);
+            } else
+            {
+                controller.setPosition(adjustedObject.position, Quaternion.Lerp(adjustedObject.rotation, initialPosition, timer), adjustedObject);
+            }
+        }
     }
 
     //An overload for the interaction to take a specific object.
@@ -41,6 +79,12 @@ public class PlayerControlInteractionClass : InteractionClass
         }
 
         isOn = !isOn;
+
+        if (adjustedObject)
+        {
+            isAdjusting = true;
+            timer = 0;
+        }
 
         if (isOn)
         {
